a7a3b8fd2358bc3c465e9c2959ff8bc56a892d95
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-09-11 03:13:27 +0200

    Setup folder structure

---
+++ src/components/hompage/hompage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Homepage() {
+  return (
+    <Container>
+      <Row>
+        <Col>1 of 3</Col>
+        <Col>2 of 3</Col>
+        <Col>3 of 3</Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Homepage;
